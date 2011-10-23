45a072233a2be7f84756cb9c9861b9a0abd2e374
Ben Woodcock <ben@playedinane.com>
2011-10-23 10:03:52 +0000

    No options set

--- ecommerce/_config.php
+++ ecommerce/_config.php
@@ -1,19 +1,3 @@
-<?php
-
-define('ECOMMERCE_DIR','ecommerce');
-
-// Extend the Member with e-commerce related fields.
-DataObject::add_extension('Member', 'EcommerceRole');
-// Extend Payment with e-commerce relationship. 
-if(!class_exists('Payment')) user_error("You need to also install the Payment module to use the eCommerce module", E_USER_ERROR);
-DataObject::add_extension('Payment', 'EcommercePayment');
-//create controller for shopping cart
-Director::addRules(50, array(
-	ShoppingCart::$URLSegment . '/$Action/$ID/$OtherID' => 'ShoppingCart'
-));
-
-Object::add_extension("DevelopmentAdmin", "EcommerceDevelopmentAdminDecorator");
-DevelopmentAdmin::$allowed_actions[] = 'ecommerce';
 
 // copy the lines below to your mysite/_config.php file and set as required.
 // __________________________________START ECOMMERCE MODULE CONFIG __________________________________
